d9f8753b4526fddacffc40b09111e5eb8fcb616a
Andrew Sipes <andrew.n.sipes@gmail.com>
2022-04-09 16:43:51 -0400

    Build JSON index of recent posts on push

---
+++ src/js/recent-posts.js
@@ -0,0 +1,20 @@
+fetch("../api/recent-posts.json")
+  .then((response) => response.json())
+  .then((articles) => buildRecent(articles));
+
+function buildRecent(articles) {
+  const target = document.querySelector("#recent-posts");
+
+  if (!target) return;
+
+  articles.forEach((article) => {
+    target.innerHTML += `
+      <article>
+        <h1>${article.title}</h1>
+        <p>${article.description}</p>
+        <p>${article.published}</p>
+        <img src="${article.img.src}" alt="${article.img.alt}" width="${article.img.width}" height="${article.img.height}" />
+      </article>
+    `;
+  });
+}
---
+++ util/build-recent-posts.js
@@ -0,0 +1,96 @@
+// Import built-in and external libraries
+const fs = require("fs");
+const {
+  promises: { readFile },
+} = require("fs");
+const path = require("path");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+// Set directory to be indexed
+const directory = "./src/posts";
+
+// Get a list of all of the files in the directory
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
+
+  // Create an array of promises for all files in directory
+  let promises = files.map((filename) => {
+    const pathToFile = path.join(directory, filename);
+    return getJson(pathToFile);
+  });
+
+  // Wait for all files to be processed
+  Promise.all(promises).then((data) => {
+    // Filter out any falsy entries
+    let articles = data.filter((d) => !!d);
+
+    // Sort the articles by date
+    articles.sort((a, b) => new Date(b.published) - new Date(a.published));
+
+    // Make the api directory if it does not yet exist
+    fs.mkdir("./src/api", { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    // Write the metadata from each article as a single JSON file
+    fs.writeFile(
+      "./src/api/recent-posts.json",
+      JSON.stringify(articles),
+      { flag: "wx" },
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+  });
+});
+
+// Meta tag helper function
+const getMeta = (dom, property) => {
+  const element = dom.window.document.querySelector(
+    `meta[property="og:${property}"]`
+  );
+  return element ? element.getAttribute("content") : null;
+};
+
+// Read HTML file and convert metadata into JSON
+const getJson = (pathToFile) => {
+  return new Promise((resolve, reject) => {
+    readFile(pathToFile, "utf8")
+      .then((html) => {
+        const dom = new JSDOM(html);
+        const title = getMeta(dom, "title") || "Untitled";
+        const description = getMeta(dom, "description") || "No description";
+        const url = getMeta(dom, "url") || "";
+        const imgSrc = getMeta(dom, "image") || "/images/no-og:image.jpg";
+        const imgWidth = getMeta(dom, "image:width") || "1600";
+        const imgHeight = getMeta(dom, "image:height") || "900";
+        const imgAlt = getMeta(dom, "image:alt") || "";
+        const published =
+          getMeta(dom, "article:published_time") || "0000-00-00";
+
+        resolve({
+          title,
+          description,
+          url,
+          img: {
+            src: imgSrc,
+            alt: imgAlt,
+            width: imgWidth,
+            height: imgHeight,
+          },
+          published,
+        });
+      })
+      .catch((error) => {
+        console.error(error.message);
+        reject();
+      });
+  });
+};
